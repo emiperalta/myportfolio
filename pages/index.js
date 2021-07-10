093ebf8119da4350eec47c850357a0291319afe4
Emiliano Peralta <emilianop96@hotmail.com>
2021-07-10 13:14:33 -0300

    changing first letter of my name to uppercase in meta

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <Head>
         <title>Emiliano Peralta / Portfolio</title>
-        <meta name='description' content='emiliano peralta portfolio' />
+        <meta name='description' content='Emiliano Peralta portfolio' />
         <link rel='shortcut icon' href='/favicon.png' type='image/x-icon' />
         <link
           rel='stylesheet'
