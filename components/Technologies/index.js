91bd83bab4902e58d96f5e611c2c6f2199b9f4e3
Emiliano <emilianop96@hotmail.com>
2022-02-22 13:53:29 -0300

    update tech icons
    - update icons size

--- components/Technologies/index.js
+++ components/Technologies/index.js
@@ -4,18 +4,19 @@ export default function Technologies() {
       <section className='technologies'>
         <h3>Tecnologías</h3>
         <div className='icons'>
-          <i className='devicon-nodejs-plain-wordmark colored'></i>
-          <i className='devicon-express-original-wordmark'></i>
-          <i className="devicon-typescript-plain colored"></i>
-          <i className="devicon-jest-plain colored"></i>
-          <i className="devicon-docker-plain-wordmark colored"></i>
-          <i className='devicon-dot-net-plain-wordmark colored'></i>
-          <i className='devicon-react-original-wordmark colored'></i>
-          <i className="devicon-redux-original colored"></i>
-          <i className='devicon-nextjs-original-wordmark'></i>
-          <i className='devicon-mongodb-plain-wordmark colored'></i>
-          <i className='devicon-mysql-plain-wordmark colored'></i>
-          <i className='devicon-git-plain-wordmark colored'></i>
+          <i className='devicon-nodejs-plain-wordmark colored' />
+          <i className='devicon-express-original-wordmark' />
+          <i className='devicon-nestjs-plain-wordmark colored' />
+          <i className='devicon-typescript-plain colored' />
+          <i className='devicon-jest-plain colored' />
+          <i className='devicon-docker-plain-wordmark colored' />
+          <i className='devicon-react-original-wordmark colored' />
+          <i className='devicon-redux-original colored' />
+          <i className='devicon-nextjs-original-wordmark' />
+          <i className='devicon-mongodb-plain-wordmark colored' />
+          <i className='devicon-mysql-plain-wordmark colored' />
+          <i className='devicon-postgresql-plain-wordmark colored' />
+          <i className='devicon-git-plain-wordmark colored' />
         </div>
       </section>
       <style jsx>{`
@@ -30,7 +31,7 @@ export default function Technologies() {
           padding: 30px 20px;
         }
         .icons i {
-          font-size: 5rem;
+          font-size: 4rem;
           margin: 10px 25px;
         }
       `}</style>
